b63b27c962e05b99452edc3bdaad41729ac741f9
Jhong Emata <20211199@nbsc.edu.ph>
2025-05-05 00:11:46 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Capsexpense',
+  webDir: 'dist'
+};
+
+export default config;
