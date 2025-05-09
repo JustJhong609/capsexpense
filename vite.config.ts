43b11342e5998ebbd7b59fe9b33206e243ba6e0e
JustJhong609 <20211199@nbsc.edu.ph>
2025-05-09 10:45:58 +0800

    Added Routes for Vite and Packagejson

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/capsexpense/", // <-- add this line!
   plugins: [
     react(),
     legacy()
@@ -16,3 +17,4 @@ export default defineConfig({
     setupFiles: './src/setupTests.ts',
   }
 })
+
