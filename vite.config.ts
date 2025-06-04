328738f8f0ea5bc40abe70af4ca142ee4668ae0c
Jhong Emata <20211199@nbsc.edu.ph>
2025-06-03 19:35:31 -0700

    Fix vite  config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  base: "/capsexpense/", // <-- add this line!
+  base: isProd ? "/capsexpense/" : "/",
   plugins: [
     react(),
     legacy()
